23d522b2d84fd9cde380d04029f0f434f3c44c0a
Dominic nix <dnix8715@gmail.com>
2024-02-13 10:11:24 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+    const headquarters = 42;
+    return Math.abs(location - headquarters);
+}
+
+
+function distanceFromHqInFeet(location) {
+    const block = 264; 
+    return distanceFromHqInBlocks(location) * block;
+}
+
+
+function distanceTravelledInFeet(start, destination) {
+    const block = 264; 
+    return Math.abs(destination - start) * block;
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+
+    if (distance <= 400) {
+        return 0; 
+    } else if (distance > 400 && distance <= 2000) {
+        return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+        return 25; 
+    } else {
+        return 'cannot travel that far'; 
+    }
+}
